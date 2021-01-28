572f8f0edcebf0a96e328f2e227983e6cb028aff
Kennan LeJeune <kennan@case.edu>
2021-01-28 12:25:20 -0500

    make sure checks actually look for a flake.nix

--- checks.sh
+++ checks.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
 for i in *; do
-    if [[ -d $i ]]; then
-        nix develop $i/ -c echo "test" || exit 1
+    if [[ -d $i ]] && [[ -e $i/flake.nix ]]; then
+        echo "testing $i"
+        nix develop $i/ -c echo "ok" || exit 1
     fi
 done
 
