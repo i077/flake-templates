48e145285e3c2c9a6d6ac98a0a668b724527c70c
Kennan LeJeune <kennan@case.edu>
2021-01-28 13:47:29 -0500

    fix accidentally adding all flakes to the failed list

--- checks.sh
+++ checks.sh
@@ -5,7 +5,7 @@ failed=()
 for i in *; do
     if [[ -d $i ]] && [[ -e $i/flake.nix ]]; then
         echo "testing $i"
-        nix develop $i/ -c echo "PASSED: $i" || echo "FAILED: $i" && failed+=($i)
+        nix develop $i/ -c echo "PASSED: $i" || (echo "FAILED: $i" && failed+=($i))
     fi
 done
 
