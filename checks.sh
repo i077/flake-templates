7007b9ea6995ccf3a006a20c3ffa9c99e2ff4ef1
Kennan LeJeune <kennan@case.edu>
2021-01-28 14:23:19 -0500

    remove redundant printing

--- checks.sh
+++ checks.sh
@@ -4,7 +4,6 @@ failed=()
 
 for i in *; do
     if [[ -d $i ]] && [[ -e $i/flake.nix ]]; then
-        echo "testing $i"
         nix develop $i/ -c echo "PASSED: $i" || (echo "FAILED: $i" && failed+=($i))
     fi
 done
